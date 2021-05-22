83bb87552eddbed49e480e988a03be73a3bc827c
Faical <faical.allou@gmail.com>
2021-05-22 13:34:02 +0200

    added images and paths

--- src/app.py
+++ src/app.py
@@ -1,5 +1,12 @@
-from flask import Flask
+from flask import Flask, send_file
+import simplejson as json
+import os, os.path
+
+
 from models.dataInterface import *
+
+# simple version for working with CWD
+print 
 
 app = Flask(__name__)
 
@@ -11,14 +18,30 @@ def hello():
     return 'Hello World! from the container'
 
 @app.route('/all')
-def all():
+def getAllrecipes():
     listRecipes = dataInterface.getAllRecipes()
     return listRecipes
 
 @app.route('/<id>')
-def recipeId(id):
+def getRecipeId(id):
     recipe = dataInterface.getRecipebyId(id)
     return recipe
+
+@app.route('/get_image/<recipeId>/count')
+def getRecipeImageCount(recipeId):
+    print(os.listdir('./assets/image/recipe-'+recipeId+'/'))
+    count = len(os.listdir('./assets/image/recipe-'+recipeId+'/'))
+    return json.dumps({'data':count})
+
+@app.route('/get_image/<recipeId>/<id>')
+def getRecipeImage(recipeId,id):
+    try:
+       filename = '../assets/image/recipe-'+recipeId+'/'+id+'.jpg'
+       return send_file(filename, mimetype='image/jpg')
+    except:
+       filename = '../assets/image/error404.gif'
+       return send_file(filename, mimetype='image/gif')
+    return 
 
 
 if __name__ == '__main__':
--- src/models/dataInterface.py
+++ src/models/dataInterface.py
@@ -1,32 +1,21 @@
 import simplejson as json
 import pandas as pd
 import pathlib
+from flask import send_file
+
 
 class dataInterface:
 
     def getAllRecipes(self ):
-
-        print(pathlib.Path().absolute())
-
         recipes = pd.read_csv('./assets/data/recipes.csv')
-        print(recipes)
         result = json.dumps(recipes.to_json(orient="records"))
         return result
 
     def getRecipebyId(self, inputId):
         recipes = pd.read_csv('./assets/data/recipes.csv')
-        print(recipes)
-
-        print(inputId)
-
         recipe = recipes[recipes['id'].astype(str) == inputId]
-        
-        print(recipe)
-
-        print(recipe.to_json(orient="records"))
-
         result = json.dumps(recipe.to_json(orient="records"))
         return result
+    
 
-def __init__(self):
-        print ("in init")
+
