b76a5f02be22946f9c1449cdc0c21b5d5b06dfc9
Faical Allou <faical.allou@gmail.com>
2021-06-03 18:49:25 +0200

    too many things to count

--- src/app.py
+++ src/app.py
@@ -1,14 +1,19 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request
 import simplejson as json
 import os, os.path
 from werkzeug.serving import WSGIRequestHandler
+import datetime
+import os.path
+import requests
 
 from models.dataInterface import *
+import config
 
 # simple version for working with CWD
 print 
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './static/image/'
 
 dataInterface = dataInterface()
 
@@ -29,19 +34,52 @@ def getRecipeId(id):
 
 @app.route('/get_image/<recipeId>/count')
 def getRecipeImageCount(recipeId):
-    count = len(os.listdir('./assets/image/recipe-'+recipeId+'/'))
+    count = len(os.listdir('./static/image/recipe-'+recipeId+'/'))
     return json.dumps({'data':count})
 
 @app.route('/get_image/<recipeId>/<id>')
 def getRecipeImage(recipeId,id):
     try:
-       filename = '../assets/image/recipe-'+recipeId+'/'+id+'.jpg'
+       filename = '../static/image/recipe-'+recipeId+'/'+id+'.jpg'
        return send_file(filename, mimetype='image/jpg')
     except:
-       filename = '../assets/image/error404.gif'
+       filename = '../static/image/error404.gif'
        return send_file(filename, mimetype='image/gif')
     return 
 
+@app.route('/get_schedule/<recipeId>/<chefId>')
+def getSchedule(recipeId,chefId):
+    query = recipeId + '-' + chefId
+    calendar = config.calendar
+    key = config.key
+    url = 'https://www.googleapis.com/calendar/v3/calendars/'+calendar+'/events?q='+query+'&key='+key
+    x = requests.get(url)
+    jsonX = x.json()
+    return jsonX
+
+@app.route('/edit_recipe/', methods=['POST'])
+def addRecipe():
+    data = dict(request.form)
+
+    
+
+    dataDF = recipe(data['chefId'],data['recipeId'],data['recipeName'],data['recipeDescription'],data['ingredients'],data['tools'])
+    if data['recipeId']=='0':
+        data['recipeId'] = str(dataInterface.getMaxRecipeId() +1)
+        dataInterface.insertRecipe(dataDF)
+    else:
+        dataInterface.updateRecipe(dataDF)
+        print('inserting')
+    
+
+    if len(request.files) > 0:
+        file = request.files['image1']
+        if os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'],'recipe-'+data['recipeId'])):
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],'recipe-'+data['recipeId'],'1.jpg'))
+        else:
+            os.mkdir(os.path.join(app.config['UPLOAD_FOLDER'],'recipe-'+data['recipeId']))
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],'recipe-'+data['recipeId'],'1.jpg'))
+    return json.dumps({'recipeId':data['recipeId'], 'chefId':data['chefId'], 'status':'success'})
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
@@ -50,4 +88,4 @@ if __name__ == '__main__':
     if os.environ.get('ON_HEROKU'):
         app.run(host='0.0.0.0', port=port)
     else :
-        app.run(host='localhost', port=port)
+        app.run(host='localhost', port=port, debug=True)
--- src/models/dataInterface.py
+++ src/models/dataInterface.py
@@ -1,22 +1,86 @@
 import simplejson as json
 import pandas as pd
 import pathlib
+import os
 from flask import send_file
+
+
+class recipe:
+    def __init__(self, chefId, recipeId, recipeName, description, ingredients, tools):
+        self.chefId = chefId
+        self.recipeId = recipeId
+        self.recipeName = recipeName
+        self.description = description
+        self.ingredients = ingredients
+        self.tools = tools
+    
+class chef:
+    def __init__(self, chefId, chefName):
+        self.chefId = chefId
+        self.chefName = chefName
+
 
 
 class dataInterface:
 
     def getAllRecipes(self ):
-        recipes = pd.read_csv('./assets/data/recipes.csv')
+        recipes = pd.read_csv('./static/data/recipes.csv')
         result = recipes.to_json(orient="records")
         return result
 
     def getRecipebyId(self, inputId):
-        recipes = pd.read_csv('./assets/data/recipes.csv')
-        recipe = recipes[recipes['id'].astype(str) == inputId]
+        recipes = pd.read_csv('./static/data/recipes.csv')
+        result = recipes[recipes['recipeId'].astype(str) == inputId]
 
-        result = recipe.to_json(orient="records")
+        result = result.to_json(orient="records")
         return result
     
+    def insertRecipe(self, recipe):
+        
+        recipes = pd.read_csv('./static/data/recipes.csv')
 
+        data = vars(recipe)
 
+        _chef = json.loads(self.getChefbyId(data['chefId']))
+        print(_chef)
+        data['chefName'] = _chef[0]['chefName']
+
+        data['recipeId'] = self.getMaxRecipeId()+1
+        df = recipes.append(data, ignore_index=True)
+        df.to_csv('./static/data/recipes.csv', index = False)
+               
+        return '1'
+
+    def updateRecipe(self, recipe):
+        
+        recipes = pd.read_csv('./static/data/recipes.csv')
+        recipes['recipeId'] =  recipes['recipeId'].astype(str)
+        data = vars(recipe)
+
+        _chef = json.loads(self.getChefbyId(data['chefId']))
+
+        data['chefName'] = _chef[0]['chefName']
+        recipeId = data['recipeId']
+        for column in recipes:
+            if data[column] != '':
+                recipes.loc[recipes['recipeId'] == recipeId, column] = data[column]
+
+        recipes.to_csv('./static/data/recipes.csv', index = False)
+               
+        return '1'
+    
+    def getChefbyId(self, inputId):
+        
+        chefs = pd.read_csv('./static/data/chefs.csv')
+        result = chefs[chefs['chefId'].astype(str) == inputId]
+
+        result = result.to_json(orient="records")
+
+        return result
+
+    def getMaxRecipeId(self):
+        
+        recipes = pd.read_csv('./static/data/recipes.csv')
+        result = len(recipes)
+
+        return result
