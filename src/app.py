ea36a3d0de4c299f47b573e61a5368521a84d64a
Faical Allou <faical.allou@gmail.com>
2021-06-13 15:51:36 +0200

    added event creation deletion

--- src/app.py
+++ src/app.py
@@ -7,6 +7,11 @@ import os.path
 import shutil
 import requests
 
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
 from models.dataInterface import *
 import config
 
@@ -29,34 +34,34 @@ def authorize(key):
         abort(404) 
         
 
-@app.route('/all')
+@app.route('/v1/all')
 def getAllrecipes():
     authorize(request.headers['Authorization'])
     listRecipes = dataInterface.getAllRecipes()
     return listRecipes
 
 
-@app.route('/recipe/<id>')
+@app.route('/v1/recipe/<id>')
 def getRecipeId(id):
     authorize(request.headers['Authorization'])
     recipe = dataInterface.getRecipebyId(id)
     return recipe
 
-@app.route('/chef/<id>')
+@app.route('/v1/chef/<id>')
 def getChefId(id):
     authorize(request.headers['Authorization'])
     chef = dataInterface.getChefbyId(id)
     print(chef)
     return chef
 
-@app.route('/get_image/<recipeId>/count')
+@app.route('/v1/get_image/<recipeId>/count')
 def getRecipeImageCount(recipeId):
     authorize(request.headers['Authorization'])
     count = len(os.listdir('./static/image/recipe-'+recipeId+'/'))
     return json.dumps({'data':count})
 
 
-@app.route('/get_image/<recipeId>/<id>')
+@app.route('/v1/get_image/<recipeId>/<id>')
 def getRecipeImage(recipeId,id):
    # authorize(request.headers['Authorization'])
     try:
@@ -67,7 +72,7 @@ def getRecipeImage(recipeId,id):
        return send_file(filename, mimetype='image/gif')
     return 
 
-@app.route('/get_image/<id>')
+@app.route('/v1/get_image/<id>')
 def getChefImage(id):
     # authorize(request.headers['Authorization'])
     try:
@@ -79,19 +84,57 @@ def getChefImage(id):
     return 
 
 
-@app.route('/get_schedule/<recipeId>/<chefId>')
-def getSchedule(recipeId,chefId):
-    authorize(request.headers['Authorization'])
-    query = recipeId + '-' + chefId
-    calendar = config.calendar
+@app.route('/v1/get_schedule/<chefId>/<recipeId>')
+def getSchedule(chefId,recipeId):
+    #authorize(request.headers['Authorization'])
+    query = chefId + '-' + recipeId 
+    calendarId = config.calendarIdpct
     key = config.key
-    url = 'https://www.googleapis.com/calendar/v3/calendars/'+calendar+'/events?q='+query+'&key='+key
+    url = 'https://www.googleapis.com/calendar/v3/calendars/'+calendarId+'/events?q='+query+'&key='+key
     x = requests.get(url)
     jsonX = x.json()
+    print(jsonX)
     return jsonX
 
+@app.route('/v1/add_event/<chefId>/<recipeId>', methods=['POST'])
+def addEvent(chefId,recipeId):
+    #authorize(request.headers['Authorization'])
+    query =  chefId + '-' + recipeId
+    data = dict(request.form)
+    event = {"summary": chefId + '-' + recipeId,
+             "description": data['eventDescription'] ,
+              "start": {
+                "dateTime": data['eventStart'],
+                "timeZone": "UTC"
+              },
+              "end": {
+                "dateTime": data['eventEnd'],
+                "timeZone": "UTC"
+              },
+              "conferenceDataVersion": 1,
+              "conferenceData": {
+                  "createRequest": {
+                        "conferenceSolutionKey": { 
+                                    "type": "hangoutsMeet", 
+                                    },
+                        "requestId": str(datetime.datetime.now()),
+                        },
+                  }
+              }
 
-@app.route('/edit_recipe/', methods=['POST'])
+    event = service.events().insert(calendarId=config.calendarIdat, body=event,  conferenceDataVersion=1).execute()
+    print ('Event created: ' +  str(event))
+    return 'event added'
+
+@app.route('/v1/delete_event/<eventId>', )
+def deleteEvent(eventId):
+    authorize(request.headers['Authorization'])
+    
+    event = service.events().delete(calendarId=config.calendarIdat, eventId=eventId).execute()
+     
+    return 'event deleted'
+
+@app.route('/v1/edit_recipe/', methods=['POST'])
 def editRecipe():
     authorize(request.headers['Authorization'])
     data = dict(request.form)
@@ -113,7 +156,7 @@ def editRecipe():
     return json.dumps({'recipeId':data['recipeId'], 'chefId':data['chefId'], 'status':'success'})
 
 
-@app.route('/edit_account/', methods=['POST'])
+@app.route('/v1/edit_account/', methods=['POST'])
 def editAccount():
     authorize(request.headers['Authorization'])
     data = dict(request.form)
@@ -136,7 +179,7 @@ def editAccount():
 
     return json.dumps({'chefId':data['chefId'], 'status':'success'})
 
-@app.route('/delete_recipe/<id>')
+@app.route('/v1/delete_recipe/<id>')
 def doDeleteRecipe(id):
     authorize(request.headers['Authorization'])
     dataInterface.deleteRecipe(id)
@@ -146,7 +189,7 @@ def doDeleteRecipe(id):
         print('problem when deleting Recipe')
     return 'deleted'
  
-@app.route('/delete_chef/<id>')
+@app.route('/v1/delete_chef/<id>')
 def doDeleteChef(id):
     authorize(request.headers['Authorization'])
     dataInterface.deleteChef(id)
@@ -161,10 +204,32 @@ def doDeleteChef(id):
 
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    # Doing the google authentification
+    SCOPES = ['https://www.googleapis.com/auth/calendar']
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    
     WSGIRequestHandler.protocol_version = "HTTP/1.1"
     if os.environ.get('ON_HEROKU'):
+        port = int(os.environ.get('PORT', 5000))
         app.run(host='0.0.0.0', port=port)
     else :
-        app.run(host='0.0.0.0', port=port, debug=True)
+        app.run(host='0.0.0.0', port=5000, debug=True)
